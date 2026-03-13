2c475c95de979c17a363c48259157a059843c972
h-nakagawa <h-nakagawa@ykc.co.jp>
2026-03-13 15:58:20 +0900

    chore: add local demo docker helper scripts

--- scripts/stop_my_docker_compose.sh
+++ scripts/stop_my_docker_compose.sh
@@ -3,6 +3,9 @@
 # Usage:
 #     1. cd live-vlm-webui
 #     2. sudo bash scripts/stop_my_docker_compose.sh
+# Info:
+#     - `./live-vlm-webui/.env`に環境変数を記述して、`docker compose`実行時に環境変数を直接渡すようにしています
+#     - 環境変数を変更する場合は、`.env`修正後、`docker compose`のサービス再起動を実行します
 # ---
 
 # docker service `live-vlm-webui`
